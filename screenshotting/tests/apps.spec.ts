2a47d928c7e2bf1b0e29cc05fb758e0e91fc8477
Mario Trost <mario.trost@bluewin.ch>
2022-11-07 14:02:02 +0100

    Create github action workflow

--- screenshotting/tests/apps.spec.ts
+++ screenshotting/tests/apps.spec.ts
@@ -2,10 +2,14 @@ import { test, expect, chromium } from '@playwright/test';
 
 test.use({
   viewport: {
-    height: 944,
+    height: 1200,
     width: 1916,
   },
 });
+
+async function delay(ms = 5000) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
 test('test', async ({}, { config }) => {
   const { baseURL } = config.projects[0].use;
@@ -20,10 +24,29 @@ test('test', async ({}, { config }) => {
 
   //////////
   // await page.goto('https://nuvla.io');
-  await page.pause();
   await expect(page).toHaveURL('https://nuvla.io/ui/welcome');
   await page.getByRole('link', { name: 'apps' }).click();
   await expect(page).toHaveURL('https://nuvla.io/ui/apps');
   await page.waitForSelector('.ui.bordered.image', { state: 'visible' });
-  await page.screenshot({ fullPage: true, path: './apps_fullpage.png' });
+
+  await delay(3000);
+  await page.screenshot({ fullPage: true, path: '../docs/assets/img/app-store.png' });
+
+  await page.getByText('Navigate Apps').click();
+
+  await page.getByText('Add').click();
+  await page.pause();
+  await page
+    // .locator('.ui.modal.transition.visible.active')
+    // .nth(1)
+    .screenshot({
+      clip: {
+        x: 460,
+        y: 390,
+        width: 1000,
+        height: 400,
+      },
+      path: '../docs/assets/img/add-new-project-new.png',
+    });
 });
+// ui modal transition visible active
